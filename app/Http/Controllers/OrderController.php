dba48ad578d6a8a2f3ccc49c07ee199dd07e5f3f
shadw77 <Shady2018170763@cis.asu.edu.eg>
2023-10-15 00:29:12 +0300

    app index for orders and make migration for orders table

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class OrderController extends Controller
 {
@@ -12,6 +13,9 @@ class OrderController extends Controller
     public function index()
     {
         //
+        $orders = Order::all();
+        return view('Orders.index',['orders'=>$orders] );
+
     }
 
     /**
