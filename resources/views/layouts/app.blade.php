11ba4b04ed8d8457a5dcc9a56c260bd6f6ef80d5
shadw77 <Shady2018170763@cis.asu.edu.eg>
2023-10-14 23:50:13 +0300

    add home and orders to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,11 +30,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <a class="nav-link" href="">Home</a>
+                        <a class="nav-link" href="">Orders</a>
                     </ul>
 
+
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav ms-auto">               
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
