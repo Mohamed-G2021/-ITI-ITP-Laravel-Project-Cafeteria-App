b2ab0af4427280f0d116aee20d2e189818dda6d8
alaanegm <alaanegm275@gmail.com>
2023-10-19 14:55:10 +0300

    merge main

--- resources/views/admins/index.blade.php
+++ resources/views/admins/index.blade.php
@@ -1,17 +1,7 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container d-flex align-items-center justify-content-center " >
-        <div class="m-3 p-3 mt-5">
-            <a href="{{ route('admin-users.create') }}" class="btn btn-success mx-2">Add New User</a>
-      
-            <table class="table table-light table-striped text-center  table-bordered my-4" style="width: 600px">
-    <thead>
-        <tr><th>Image</th> <th>Name</th> <th>Email</th> <th>Edit</th> <th>Delete</th></tr>
-    </thead>
-    <tbody>
-            @foreach($users as $user)
 
-<div class="container ">
+   <div class="container ">
   <a href="{{route('admin-users.create') }}" class="btn btn-success mt-3">Add New User</a>
     <div class="row justify-content-center fw-bolder">
     <div class="col mt-4">
@@ -43,14 +33,10 @@
                     </td>
                 </tr>
                 @endforeach
-
             </tbody>
         </table>
    </div>
-   <div class="d-flex pagination">
-    {!! $users->links() !!}
-     </div>
+  
     </div>
-   
-        @endsection
+  @endsection
 
