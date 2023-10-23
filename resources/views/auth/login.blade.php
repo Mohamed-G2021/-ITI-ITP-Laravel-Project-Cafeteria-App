075641c7ca791ae67ff7dc3a48f450f90653bec9
shadw77 <Shady2018170763@cis.asu.edu.eg>
2023-10-23 21:13:03 +0300

    enhancing ui #7

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<style>
+      .main{
+        background-color:#823a35;
+        color:white;
+        height:100vh;
+    }
+</style>
+<div class="container main">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -53,7 +60,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-warning">
                                     {{ __('Login') }}
                                 </button>
 
